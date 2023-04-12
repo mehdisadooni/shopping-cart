4f18a8e06e95dd422159238faa80cc759f893acb
Mehdi sadooni <mdi.mehdi97@gmail.com>
2023-04-13 00:58:00 +0430

    install redux and use it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 function App() {
     return (
         <>
             <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                </Routes>
+                <Provider store={store}>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                    </Routes>
+                </Provider>
             </BrowserRouter>
         </>
     );
